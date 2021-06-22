54b8efa8c257c20910adf6c37268f18e4ce3967f
greyhoundforty <yantiffany@fastmail.com>
2021-06-22 08:13:07 -0500

    Fixes #1
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

--- security/outputs.tf
+++ security/outputs.tf
@@ -1,3 +1,7 @@
 output "wireguard_security_group" {
   value = ibm_is_security_group.wireguard_security_group.id
 }
+
+output "internal_security_group" {
+  value = ibm_is_security_group.internal_security_group.id
+}
