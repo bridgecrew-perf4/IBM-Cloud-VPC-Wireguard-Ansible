1f9d9a2837798bd3a745d125407d013e62c3e01f
Ryan Tiffany <ryantiffany@fastmail.com>
2022-05-25 09:19:30 -0500

    Initial clean up for updated playbook

--- ansible/main.tf
+++ ansible/main.tf
@@ -13,20 +13,13 @@ resource "local_file" "ansible-vars" {
     {
       client_peer_allowed_ips = var.client_peer_allowed_ips
       client_public_key       = var.client_public_key
+      local_ip                = var.local_ip
+      wireguard_instance      = var.wireguard_instance
+      backend_subnet_cidr     = var.backend_subnet_cidr
+      frontend_subnet_cidr    = var.frontend_subnet_cidr
     }
   )
   filename = "${path.module}/playbooks/vars.yml"
 }
 
-resource "local_file" "wireguard-local-template" {
-  content = templatefile("${path.module}/templates/wg0.client.j2",
-    {
-      local_ip             = var.local_ip
-      wireguard_instance   = var.wireguard_instance
-      backend_subnet_cidr  = var.backend_subnet_cidr
-      frontend_subnet_cidr = var.frontend_subnet_cidr
-    }
-  )
-  filename = "${path.module}/updated_client_config"
-}
 
